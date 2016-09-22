b667e08734e0af83291fffa41e1ee4ecae302f3d
chaiwat <mynameistecs51@gmail.com>
2016-09-22 12:38:23 +0700

    update team

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -285,235 +285,141 @@
 			<div class="row">
 				<div class="col-lg-12 text-center">
 					<h2 class="section-heading">คณะทำงาน</h2>
-					<h3 class="section-subheading text-muted">คณะทำงานโครงการฯ.</h3>
+					<h3 class="section-subheading text-muted">คณะดำเนินงาน โครงการฝึกอบรมส่งเสริมอาชีพเพื่อแก้ปัญหาความยากจน ประกอบไปด้วย</h3>
 				</div>
-			</div>
-			<div class="row">
-				<div class="col-sm-3">
-					<div class="team-member">
-						<img src="<?php echo base_url();?>assets/img/team/rutthichai.jpg" class="img-responsive img-circle" alt="" style="width: 100px;height:130px;">
-						<h5>ผศ.ดร.น.สพ.ฤทธิชัย  พิลาไชย</h5>
-						<p class="text-muted">อาจารย์</p>
-						<!-- <ul class="list-inline social-buttons">
-							<li><a href="#"><i class="fa fa-twitter"></i></a>
-							</li>
-							<li><a href="#"><i class="fa fa-facebook"></i></a>
-							</li>
-							<li><a href="#"><i class="fa fa-linkedin"></i></a>
-							</li>
-						</ul> -->
-					</div>
-				</div>
-				<div class="col-sm-3">
-					<div class="team-member">
-						<img src="<?php echo base_url();?>assets/img/team/uttasart.jpg" class="img-responsive img-circle" alt="" style="width: 100px;height:130px;">
-						<h5>ผศ.ดร.อรรถศาสตร์  วิเศียรศาสตร์</h5>
-						<p class="text-muted">อาจารย์</p>
-						<!-- <ul class="list-inline social-buttons">
-							<li><a href="#"><i class="fa fa-twitter"></i></a>
-							</li>
-							<li><a href="#"><i class="fa fa-facebook"></i></a>
-							</li>
-							<li><a href="#"><i class="fa fa-linkedin"></i></a>
-							</li>
-						</ul> -->
-					</div>
-				</div>
-				<div class="col-sm-3">
-					<div class="team-member">
-						<img src="<?php echo base_url();?>assets/img/team/krit.jpg" class="img-responsive img-circle" alt="" style="width: 100px;height:130px;">
-						<h5>ผศ.กฤตฏ์ ชมภูวิเศษ</h5>
-						<p class="text-muted">อาจารย์</p>
-						<!-- <ul class="list-inline social-buttons">
-							<li><a href="#"><i class="fa fa-twitter"></i></a>
-							</li>
-							<li><a href="#"><i class="fa fa-facebook"></i></a>
-							</li>
-							<li><a href="#"><i class="fa fa-linkedin"></i></a>
-							</li>
-						</ul> -->
-					</div>
-				</div>
-				<div class="col-sm-3">
-					<div class="team-member">
-						<img src="<?php echo base_url();?>assets/img/team/kanisorn.jpg" class="img-responsive img-circle" alt="" style="width: 100px;height:130px;">
-						<h5>ผศ.ดร.คณิศร  จี้กระโทก</h5>
-						<p class="text-muted">อาจารย์</p>
-						<!-- <ul class="list-inline social-buttons">
-							<li><a href="#"><i class="fa fa-twitter"></i></a>
-							</li>
-							<li><a href="#"><i class="fa fa-facebook"></i></a>
-							</li>
-							<li><a href="#"><i class="fa fa-linkedin"></i></a>
-							</li>
-						</ul> -->
-					</div>
-				</div>
-				<div class="col-sm-3">
-					<div class="team-member">
-						<img src="<?php echo base_url();?>assets/img/team/patcharin.jpg" class="img-responsive img-circle" alt="" style="width: 100px;height:130px;">
-						<h5>ดร.พัชรินทร์  ชมภูวิเศษ</h5>
-						<p class="text-muted">อาจารย์</p>
-						<!-- <ul class="list-inline social-buttons">
-							<li><a href="#"><i class="fa fa-twitter"></i></a>
-							</li>
-							<li><a href="#"><i class="fa fa-facebook"></i></a>
-							</li>
-							<li><a href="#"><i class="fa fa-linkedin"></i></a>
-							</li>
-						</ul> -->
-					</div>
-				</div>
-				<div class="col-sm-3">
-					<div class="team-member">
-						<img src="<?php echo base_url();?>assets/img/team/wachirawut.jpg" class="img-responsive img-circle" alt="" style="width: 100px;height:130px;">
-						<h5>อาจารย์วชิราวุธ  พิศยะไตร</h5>
-						<p class="text-muted">อาจารย์</p>
-						<!-- <ul class="list-inline social-buttons">
-							<li><a href="#"><i class="fa fa-twitter"></i></a>
-							</li>
-							<li><a href="#"><i class="fa fa-facebook"></i></a>
-							</li>
-							<li><a href="#"><i class="fa fa-linkedin"></i></a>
-							</li>
-						</ul> -->
-					</div>
-				</div>
-				<div class="col-sm-3">
-					<div class="team-member">
-						<img src="<?php echo base_url();?>assets/img/team/nopicture.png" class="img-responsive img-circle" alt="" style="width: 100px;height:130px;">
-						<h5>อาจารย์ศิริภรณ์  โคตะมี</h5>
-						<p class="text-muted">อาจารย์</p>
-						<!-- <ul class="list-inline social-buttons">
-							<li><a href="#"><i class="fa fa-twitter"></i></a>
-							</li>
-							<li><a href="#"><i class="fa fa-facebook"></i></a>
-							</li>
-							<li><a href="#"><i class="fa fa-linkedin"></i></a>
-							</li>
-						</ul> -->
-					</div>
-				</div>
-				<div class="col-sm-3">
-					<div class="team-member">
-						<img src="<?php echo base_url();?>assets/img/team/verachai.jpg" class="img-responsive img-circle" alt="" style="width: 100px;height:130px;">
-						<h5>อาจารย์วีระชัย  ทองดี</h5>
-						<p class="text-muted">อาจารย์</p>
-						<!-- <ul class="list-inline social-buttons">
-							<li><a href="#"><i class="fa fa-twitter"></i></a>
-							</li>
-							<li><a href="#"><i class="fa fa-facebook"></i></a>
-							</li>
-							<li><a href="#"><i class="fa fa-linkedin"></i></a>
-							</li>
-						</ul> -->
-					</div>
-				</div>
-				<div class="col-sm-3">
-					<div class="team-member">
-						<img src="<?php echo base_url();?>assets/img/team/nopicture.png" class="img-responsive img-circle" alt="" style="width: 100px;height:130px;">
-						<h5>อาจารย์เยาวพล  ชุมพล</h5>
-						<p class="text-muted">อาจารย์</p>
-						<!-- <ul class="list-inline social-buttons">
-							<li><a href="#"><i class="fa fa-twitter"></i></a>
-							</li>
-							<li><a href="#"><i class="fa fa-facebook"></i></a>
-							</li>
-							<li><a href="#"><i class="fa fa-linkedin"></i></a>
-							</li>
-						</ul> -->
-					</div>
-				</div>
-				<div class="col-sm-3">
-					<div class="team-member">
-						<img src="<?php echo base_url();?>assets/img/team/jaturon.jpg" class="img-responsive img-circle" alt="" style="width: 100px;height:130px;">
-						<h5>อาจารย์จาตุรนต์  สุขุมาลพงษ์</h5>
-						<p class="text-muted">อาจารย์</p>
-						<!-- <ul class="list-inline social-buttons">
-							<li><a href="#"><i class="fa fa-twitter"></i></a>
-							</li>
-							<li><a href="#"><i class="fa fa-facebook"></i></a>
-							</li>
-							<li><a href="#"><i class="fa fa-linkedin"></i></a>
-							</li>
-						</ul> -->
-					</div>
-				</div>
-				<div class="col-sm-3">
-					<div class="team-member">
-						<img src="<?php echo base_url();?>assets/img/team/kanok-on.jpg" class="img-responsive img-circle" alt="" style="width: 100px;height:130px;">
-						<h5>อาจารย์กนกอร  สุขุมาลพงษ์</h5>
-						<p class="text-muted">อาจารย์</p>
-						<!-- <ul class="list-inline social-buttons">
-							<li><a href="#"><i class="fa fa-twitter"></i></a>
-							</li>
-							<li><a href="#"><i class="fa fa-facebook"></i></a>
-							</li>
-							<li><a href="#"><i class="fa fa-linkedin"></i></a>
-							</li>
-						</ul> -->
-					</div>
-				</div>
-				<div class="col-sm-3">
-					<div class="team-member">
-						<img src="<?php echo base_url();?>assets/img/team/nopicture.png" class="img-responsive img-circle" alt="" style="width: 100px;height:130px;">
-						<h5>นายวัชรพล ชุมแวงวาปี</h5>
-						<p class="text-muted">อาจารย์</p>
-						<!-- <ul class="list-inline social-buttons">
-							<li><a href="#"><i class="fa fa-twitter"></i></a>
-							</li>
-							<li><a href="#"><i class="fa fa-facebook"></i></a>
-							</li>
-							<li><a href="#"><i class="fa fa-linkedin"></i></a>
-							</li>
-						</ul> -->
-					</div>
-				</div>
-				<div class="col-sm-3">
-					<div class="team-member">
-						<img src="<?php echo base_url();?>assets/img/team/utcharapon.jpg" class="img-responsive img-circle" alt="" style="width: 100px;height:130px;">
-						<h5>นางสาวอัจฏราภรณ์ กรแก้ว</h5>
-						<p class="text-muted">อาจารย์</p>
-						<!-- <ul class="list-inline social-buttons">
-							<li><a href="#"><i class="fa fa-twitter"></i></a>
-							</li>
-							<li><a href="#"><i class="fa fa-facebook"></i></a>
-							</li>
-							<li><a href="#"><i class="fa fa-linkedin"></i></a>
-							</li>
-						</ul> -->
-					</div>
-				</div>
-				<div class="col-sm-3">
-					<div class="team-member">
-						<img src="<?php echo base_url();?>assets/img/team/nopicture.png" class="img-responsive img-circle" alt="" style="width: 100px;height:130px;">
-						<h5>นายวัชรพล  จันทะเนาว์</h5>
-						<p class="text-muted">อาจารย์</p>
-						<!-- <ul class="list-inline social-buttons">
-							<li><a href="#"><i class="fa fa-twitter"></i></a>
-							</li>
-							<li><a href="#"><i class="fa fa-facebook"></i></a>
-							</li>
-							<li><a href="#"><i class="fa fa-linkedin"></i></a>
-							</li>
-						</ul> -->
-					</div>
-				</div>
-				<div class="col-sm-3">
-					<div class="team-member">
-						<img src="<?php echo base_url();?>assets/img/team/chaiwat.jpg" class="img-responsive img-circle" alt="" style="width: 100px;height:130px;">
-						<h5>นายไชยวัฒน์  หอมแสง</h5>
-						<p class="text-muted">นักวิขาการคอมพิวเตอร์</p>
-						<!-- <ul class="list-inline social-buttons">
-							<li><a href="#"><i class="fa fa-twitter"></i></a>
-							</li>
-							<li><a href="#"><i class="fa fa-facebook"></i></a>
-							</li>
-							<li><a href="#"><i class="fa fa-linkedin"></i></a>
-							</li>
-						</ul> -->
-					</div>
-				</div>
+				<table class="table" width="100%">
+					<tr>
+						<td align="center">
+							<h3><b>คณะดำเนินงาน</b></h3>
+						</td>
+					</tr>
+					<tr>
+						<td>
+							<div class="col-sm-2"></div>
+							<div class="col-sm-3">
+								<div class="team-member">
+									<img src="<?php echo base_url();?>assets/img/team/krit.jpg" class="img-responsive img-circle" alt="" style="width: 100px;height:130px;">
+									<h5>ผศ.กฤตฏ์ ชมภูวิเศษ</h5>
+									<p class="text-muted"><b>หัวหน้าโครงการ</b></p>
+								</div>
+							</div>
+							<div class="col-sm-3">
+								<div class="team-member">
+									<img src="<?php echo base_url();?>assets/img/team/nopicture.png" class="img-responsive img-circle" alt="" style="width: 100px;height:130px;">
+									<h5>ดร.ปรียา เรืองทรัพย์</h5>
+									<p class="text-muted">อาจารย์</p>
+								</div>
+							</div>
+							<div class="col-sm-3">
+								<div class="team-member">
+									<img src="<?php echo base_url();?>assets/img/team/patcharin.jpg" class="img-responsive img-circle" alt="" style="width: 100px;height:130px;">
+									<h5>ดร.พัชรินทร์  ชมภูวิเศษ</h5>
+									<p class="text-muted">อาจารย์</p>
+								</div>
+							</div>
+						</td>
+					</tr>
+					<tr>
+						<td align="center"><h3><b>คณะวิทยากร</b></h3></td>
+					</tr>
+					<tr>
+						<td>
+							<div class="col-sm-3">
+								<div class="team-member">
+									<img src="<?php echo base_url();?>assets/img/team/rutthichai.jpg" class="img-responsive img-circle" alt="" style="width: 100px;height:130px;">
+									<h5>ผศ.ดร.น.สพ.ฤทธิชัย  พิลาไชย</h5>
+									<p class="text-muted">อาจารย์</p>
+								</div>
+							</div>
+							<div class="col-sm-3">
+								<div class="team-member">
+									<img src="<?php echo base_url();?>assets/img/team/uttasart.jpg" class="img-responsive img-circle" alt="" style="width: 100px;height:130px;">
+									<h5>ผศ.ดร.อรรถศาสตร์  วิเศียรศาสตร์</h5>
+									<p class="text-muted">อาจารย์</p>
+								</div>
+							</div>
+
+							<div class="col-sm-3">
+								<div class="team-member">
+									<img src="<?php echo base_url();?>assets/img/team/kanisorn.jpg" class="img-responsive img-circle" alt="" style="width: 100px;height:130px;">
+									<h5>ผศ.ดร.คณิศร  จี้กระโทก</h5>
+									<p class="text-muted">อาจารย์</p>
+								</div>
+							</div>
+
+							<div class="col-sm-3">
+								<div class="team-member">
+									<img src="<?php echo base_url();?>assets/img/team/wachirawut.jpg" class="img-responsive img-circle" alt="" style="width: 100px;height:130px;">
+									<h5>อาจารย์วชิราวุธ  พิศยะไตร</h5>
+									<p class="text-muted">อาจารย์</p>
+								</div>
+							</div>
+							<div class="col-sm-3">
+								<div class="team-member">
+									<img src="<?php echo base_url();?>assets/img/team/nopicture.png" class="img-responsive img-circle" alt="" style="width: 100px;height:130px;">
+									<h5>อาจารย์ศิริภรณ์  โคตะมี</h5>
+									<p class="text-muted">อาจารย์</p>
+								</div>
+							</div>
+							<div class="col-sm-3">
+								<div class="team-member">
+									<img src="<?php echo base_url();?>assets/img/team/verachai.jpg" class="img-responsive img-circle" alt="" style="width: 100px;height:130px;">
+									<h5>อาจารย์วีระชัย  ทองดี</h5>
+									<p class="text-muted">อาจารย์</p>
+								</div>
+							</div>
+							<div class="col-sm-3">
+								<div class="team-member">
+									<img src="<?php echo base_url();?>assets/img/team/nopicture.png" class="img-responsive img-circle" alt="" style="width: 100px;height:130px;">
+									<h5>อาจารย์เยาวพล  ชุมพล</h5>
+									<p class="text-muted">อาจารย์</p>
+								</div>
+							</div>
+							<div class="col-sm-3">
+								<div class="team-member">
+									<img src="<?php echo base_url();?>assets/img/team/jaturon.jpg" class="img-responsive img-circle" alt="" style="width: 100px;height:130px;">
+									<h5>อาจารย์จาตุรนต์  สุขุมาลพงษ์</h5>
+									<p class="text-muted">อาจารย์</p>
+								</div>
+							</div>
+							<div class="col-sm-3">
+								<div class="team-member">
+									<img src="<?php echo base_url();?>assets/img/team/kanok-on.jpg" class="img-responsive img-circle" alt="" style="width: 100px;height:130px;">
+									<h5>อาจารย์กนกอร  สุขุมาลพงษ์</h5>
+									<p class="text-muted">อาจารย์</p>
+								</div>
+							</div>
+							<div class="col-sm-3">
+								<div class="team-member">
+									<img src="<?php echo base_url();?>assets/img/team/nopicture.png" class="img-responsive img-circle" alt="" style="width: 100px;height:130px;">
+									<h5>นายวัชรพล ชุมแวงวาปี</h5>
+									<p class="text-muted">อาจารย์</p>
+								</div>
+							</div>
+							<div class="col-sm-3">
+								<div class="team-member">
+									<img src="<?php echo base_url();?>assets/img/team/utcharapon.jpg" class="img-responsive img-circle" alt="" style="width: 100px;height:130px;">
+									<h5>นางสาวอัจฏราภรณ์ กรแก้ว</h5>
+									<p class="text-muted">อาจารย์</p>
+								</div>
+							</div>
+							<div class="col-sm-3">
+								<div class="team-member">
+									<img src="<?php echo base_url();?>assets/img/team/nopicture.png" class="img-responsive img-circle" alt="" style="width: 100px;height:130px;">
+									<h5>นายวัชรพล  จันทะเนาว์</h5>
+									<p class="text-muted">อาจารย์</p>
+								</div>
+							</div>
+							<div class="col-sm-3">
+								<div class="team-member">
+									<img src="<?php echo base_url();?>assets/img/team/chaiwat.jpg" class="img-responsive img-circle" alt="" style="width: 100px;height:130px;">
+									<h5>นายไชยวัฒน์  หอมแสง</h5>
+									<p class="text-muted">นักวิขาการคอมพิวเตอร์</p>
+								</div>
+							</div>
+						</td>
+					</tr>
+				</table>
 			</div>
 			<div class="row">
 				<div class="col-lg-8 col-lg-offset-2 text-center">
